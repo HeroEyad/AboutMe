5285999397df16b6e2c2e9d84c57973fe89d93ad
HeroEyad <hero@heroeyad.xyz>
2025-08-13 18:51:26 +0300

    ill do this later

---
+++ source/backend/Paths.hx
@@ -0,0 +1,15 @@
+package backend;
+
+#if desktop
+import hxFileManager.FileManager;
+#else
+import openfl.utils.Assets;
+#end
+
+class Constants {
+    public static var EXT_IMAGE = ".png";
+}
+
+class Paths {
+    
+}
